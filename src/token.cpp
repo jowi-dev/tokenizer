5c51176a5d26c478c3cc711c456a9a01d990c0f6
jowi-dev <joey8williams@gmail.com>
2024-10-05 14:13:01 -0400

    remove unnecessary function

--- src/token.cpp
+++ src/token.cpp
@@ -19,24 +19,6 @@ enum class TokenType {
   RPAREN 
 };
 
-constexpr const char* getTokenLiteral(TokenType type) {
-  switch(type) {
-    case TokenType::ILLEGAL: return "ILLEGAL";
-    case TokenType::FILEEND: return "EOF";
-    case TokenType::NEWLINE: return "\n";
-    case TokenType::DEFINE: return "def";
-    case TokenType::DO: return "do";
-    case TokenType::END: return "end";
-    case TokenType::IDENT: return "IDENT"; // Identifie
-    case TokenType::INT: return "INT"; //Integer litera
-    case TokenType::ASSIGN: return "=";
-    case TokenType::PLUS: return "+";
-    case TokenType::LPAREN: return "(";
-    case TokenType::RPAREN: return ")";
-    default: return "ILLEGAL";
-  }
-}
-
 class Token {
   public:
     TokenType type;
