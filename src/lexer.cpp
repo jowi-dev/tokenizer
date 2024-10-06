f32903d9020a2dca4743b90a1a4bb636a6c61981
jowi-dev <joey8williams@gmail.com>
2024-10-06 13:03:49 -0400

    back to green on all but new test

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -14,6 +14,9 @@ bool isInteger(std::string literal) {
   return false;
 }
 
+bool isLiteral (char token) {
+  return  (token & ~32) - 'A' < 26 || token == '_';
+}
 bool isLiteral(std::string token) {
   std::regex pattern("[A-Za-z_]");
   return std::regex_search(token, pattern);
@@ -47,9 +50,37 @@ std::vector<Token> tokenize(std::string expression) {
   std::vector<Token> parsed;
   std::vector<std::string> words = split(expression, ' ');
   for(int i = 0; i < words.size(); i++){
-    TokenType type = getTokenType(words[i]);
-    Token* token = new Token(type, words[i]);
-    parsed.push_back(*token);
+    // If the string contains non-alpha characters or special characters that are not _, we need to parse by character
+    if(!isLiteral(words[i]) && !isInteger(words[i]) && words[i].length() > 1) {
+      // start position of identifier
+      int start_pos = 0;
+      for(int j = 0; j < words[i].length(); j++){
+        //if the current character is non-alpha, it is a token type we can parse
+        if(!isLiteral(words[i][j])){
+          std::string term = std::to_string(words[i][j]);
+          TokenType type = getTokenType(term);
+          Token* token = new Token(type, term);
+          parsed.push_back(*token);
+
+          //the position of the next potential identifier is after this term
+          start_pos = j+1;
+
+
+        } 
+        //peak the next term to see if it is alpha. if not, we have reached the end of the identifier
+        else if(!isLiteral(words[i][j+1])){
+          std::string term = words[i].substr(start_pos, j-start_pos);
+          TokenType type = getTokenType(term);
+          Token* token = new Token(type, term);
+          parsed.push_back(*token);
+        } 
+        //If neither term hits, we are still in an identifier
+      }
+    } else {
+        TokenType type = getTokenType(words[i]);
+        Token* token = new Token(type, words[i]);
+        parsed.push_back(*token);
+    }
   }
 
   //string.split whitespace
@@ -58,3 +89,6 @@ std::vector<Token> tokenize(std::string expression) {
 
   return parsed;
 }
+
+
+
