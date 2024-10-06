d44c2dd4b53abaf10cef9a8e203d50f753512d0d
jowi-dev <joey8williams@gmail.com>
2024-10-06 13:54:27 -0400

    debug symbols to test run

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-g++ -std=c++20 test/test.cpp $(pkg-config --cflags --libs catch2-with-main) -o build/test_executable
+#g++ -std=c++20 test/test.cpp $(pkg-config --cflags --libs catch2-with-main) -o build/test_executable
+g++ -std=c++20 -g -O0 -Wall -Wextra test/test.cpp $(pkg-config --cflags --libs catch2-with-main) -o build/test_executable
 
 ./build/test_executable
