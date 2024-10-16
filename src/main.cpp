ca5e06d3ade0bbb19f75b9d01796c46a23c97784
jowi-dev <joey8williams@gmail.com>
2024-10-16 11:24:04 -0400

    beginning of a repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
-#include <cstdio>
+#include <iostream>
+#include <string>
+
+#include "./lexer.cpp"
 
 int main() {
+    std::string userInput;
+    
+    while (true) {
+        std::cout << "Enter a line of text (or 'quit' to exit): ";
+        std::getline(std::cin, userInput);
+        
+        if (userInput == "quit") {
+            break;
+        }
 
-  printf("Hello World");
-
-  return 0;
+        auto tokens = tokenize(userInput);
+        
+        std::cout << "You entered: " << userInput << std::endl;
+        for(int i = 0; i < tokens.size(); i++){
+          std::cout << "{" << tokens[i].literal << ", " <<tokens[i].toString() << "}" << std::endl;
+        }
+    }
+    
+    return 0;
 }
--- src/token.cpp
+++ src/token.cpp
@@ -16,8 +16,29 @@ enum class TokenType {
   PLUS ,
   // Delimiters
   LPAREN ,
-  RPAREN 
+  RPAREN,
+  COUNT
 };
+
+// Array of strings corresponding to enum values
+constexpr std::array<std::string_view, static_cast<size_t>(TokenType::COUNT)> TokenNames = {
+  "ILLEGAL", 
+  "FILEEND",
+  "NEWLINE", 
+  "DEFINE",
+  "DO", 
+  "END",
+  "IDENT" ,
+  "INT",
+  "ASSIGN" ,
+  "PLUS" ,
+  "LPAREN" ,
+  "RPAREN" 
+};
+// Function to convert enum to string
+std::string TokenToString(TokenType token) {
+    return std::string(TokenNames[static_cast<size_t>(token)]);
+}
 
 class Token {
   public:
@@ -29,4 +50,8 @@ class Token {
       literal = lit;
     }
     auto operator<=>(const Token&) const = default;
+
+    std::string toString(){
+      return TokenToString(this->type);
+    }
 };
